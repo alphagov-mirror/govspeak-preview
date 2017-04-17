a67367d866c018c607b871dc5003f3d3ee3fba16
Chao Xian <dev@kelvingan.co.uk>
2017-04-17 22:59:04 +0100

    Add missing secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 GovspeakPreview::Application.config.secret_token = '2c74f59fa5764c27cd88a582e01a6ce0ac0d21cf36eb144a15de83c245f11b79bfc19b1680f2a28bd8e43ef4ea0a84c4b99b72041cd0d519a82acb9a81ba2aa7'
+GovspeakPreview::Application.config.secret_key_base = '8d08ab8fc57688d589097386285baf391e0de4c8ba6e4c29368c80b5d33bcccc0bc53432550143962caadea40e954f3f8945f39a4c28cc3287dac287c84a5ece'
